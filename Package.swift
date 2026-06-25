bfe8bcfdb521479f6e2ab3c1837c47c73f498cd2
ideviftekhar <ideviftekhar@gmail.com>
2026-06-25 18:25:59 +0530

    - Updated Package.swift

--- Package.swift
+++ Package.swift
@@ -3,24 +3,26 @@
 import PackageDescription
 
 let package = Package(
-    name: "IQPaywallViewController",
+    name: "IQStoreKitManager",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v15)
     ],
     products: [
         .library(
-            name: "IQPaywallViewController",
-            targets: ["IQPaywallViewController"]
+            name: "IQStoreKitManager",
+            targets: ["IQStoreKitManager"]
         )
     ],
     targets: [
-        .target(name: "IQPaywallViewController",
-            path: "IQPaywallViewController",
+        .target(name: "IQStoreKitManager",
+            path: "IQStoreKitManager",
             resources: [
                 .copy("Assets/PrivacyInfo.xcprivacy")
             ],
             linkerSettings: [
-                .linkedFramework("UIKit")
+                .linkedFramework("StoreKit"),
+                .linkedFramework("Foundation"),
+                .linkedFramework("Security")
             ]
         )
     ]
